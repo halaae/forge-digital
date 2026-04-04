c02d48714c056f8aa2a5960e2e8e1def423087f7
halaae <ayishatp78@gmail.com>
2026-04-05 01:22:45 +0530

    Add draggable before and after Transformation component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { MessageCircle } from 'lucide-react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Transformation from './components/Transformation';
 import WhyAts from './components/WhyAts';
 import WhyPortfolio from './components/WhyPortfolio';
 import ChooseUs from './components/ChooseUs';
@@ -45,6 +46,7 @@ function App() {
         <Hero />
         <Marquee />
         <Services />
+        <Transformation />
         <WhyAts />
         <WhyPortfolio />
         <ChooseUs />
